75e1fd92c76e7b9afe896b41449dccbb22fc6028
helay2333 <2793146849@qq.com>
2022-04-16 23:56:19 +0800

    Cirlequeue/deque

--- Code/src/_DS/Queue/CirleDeque.java
+++ Code/src/_DS/Queue/CirleDeque.java
@@ -2,14 +2,13 @@ package _DS.Queue;
 
 /**
  * @author Green写代码
- * @date 2022-04-16 23:05
+ * @date 2022-04-16 23:37
  */
-public class CirleQueue <E>{
+public class CirleDeque <E>{
     private int size;
     private E[] elements;
     private int front = 0;//队头下标
-    public CirleQueue(){
-
+    public CirleDeque(){
         elements = (E[]) new Object[10];
     }
     public int size(){
@@ -31,19 +30,44 @@ public class CirleQueue <E>{
         }
         front = 0;
     }
-    public void enQueue(E element){
+    //队尾添加
+    public void enQueueRear(E element){
         ensureCapacity(size+1);
         elements[(front+size)%elements.length] = element;
         size++;
     }
-    public E deQueue(){
+    //出队头部
+    public E deQueueFront(){
         E frontEle = elements[front];
         elements[front] = null;
         front = (front+1)%elements.length;//保证指向队头
         size--;
         return frontEle;
     }
+    public void enQueueFront(E element){
+        ensureCapacity(size+1);
+        front = index(-1);
+        elements[front] = element;
+        size++;
+    }
+    private int index(int index){
+        index += front;
+        if(index < 0){
+            return index + elements.length;
+        }
+        return index % elements.length;
+    }
+    public E deQueueRear(){
+        int rearIndex = index(size - 1);
+        E rear = elements[rearIndex];
+        elements[rearIndex] = null;
+        size--;
+        return rear;
+    }
     public E front(){
         return elements[front];
+    }
+    public E rear(){
+        return elements[(front+size-1)%elements.length];
     }
 }
