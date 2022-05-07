c574ed51dde56d3f9cbd13842f2b9e7cc37ab37b
helay2333 <2793146849@qq.com>
2022-05-07 17:26:12 +0800

    小顶堆

--- Code/src/_DS/Heap/Main.java
+++ Code/src/_DS/Heap/Main.java
@@ -3,13 +3,30 @@ package _DS.Heap;
 import _DS.printer.BinaryTreeInfo;
 import _DS.printer.BinaryTrees;
 
+import java.util.Comparator;
+
 /**
  * @author Green写代码
  * @date 2022-05-06 19:30
  */
 public class Main {
     public static void main(String[] args) {
-
+        int[] data = {55,2,77,22,88,99,23,121,54};
+        BinaryHeap<Integer> heap = new BinaryHeap<>( new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int k = 3;
+        for(int i = 0; i < data.length; i++) {
+            if(heap.size() < k) {
+                heap.add(data[i]);
+            } else if(data[i] > heap.get()) {
+                heap.replace(data[i]);//每一次替换都将堆中小的数字剔除了
+            }
+        }
+        BinaryTrees.println(heap);
     }
     public static void main1(String[] args) {
         BinaryHeap<Integer> heap = new BinaryHeap<>();
